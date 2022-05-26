07979d4987b71da8683d278f93e4343f53fd4790
shello <marcello.menjivar@gmail.com>
2022-05-26 02:01:03 -0600

    Terminando leccion Ticks...

---
+++ 02 - thePhilosophersStone/thePhilosophersStone/Intermediate/Build/Linux/B4D820EA/UnrealEditor/Inc/thePhilosophersStone/thePhilosophersStone.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodethePhilosophersStone_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_thePhilosophersStone;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_thePhilosophersStone()
+	{
+		if (!Z_Registration_Info_UPackage__Script_thePhilosophersStone.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/thePhilosophersStone",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x9F29CBB5,
+				0x7FF3404D,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_thePhilosophersStone.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_thePhilosophersStone.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_thePhilosophersStone(Z_Construct_UPackage__Script_thePhilosophersStone, TEXT("/Script/thePhilosophersStone"), Z_Registration_Info_UPackage__Script_thePhilosophersStone, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x9F29CBB5, 0x7FF3404D));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ 02 - thePhilosophersStone/thePhilosophersStone/Source/thePhilosophersStone/MovingPlatform.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class THEPHILOSOPHERSSTONE_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	
+	
+	
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	//Constructor del vector de la plataforma
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(-2280.0,-1930.0,95);
+	
+	UPROPERTY(EditAnywhere)
+	float MyX = 0;
+};
