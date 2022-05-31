ebb17adf6680eeac79e55d72c0360dd1026d5ecb
shello <marcello.menjivar@gmail.com>
2022-05-31 01:40:11 -0600

    Pasando el proyecto a 5.0.2

---
+++ 02 - thePhilosophersStone/thePhilosophersStone/Intermediate/ProjectFiles/cmake-headers-project.cmake
@@ -0,0 +1,5 @@
+set(PROJECT_HEADER_FILES 
+	"/home/shello/Documents/labProgramacionIII/02 - thePhilosophersStone/thePhilosophersStone/Source/thePhilosophersStone/thePhilosophersStone.h"
+	"/home/shello/Documents/labProgramacionIII/02 - thePhilosophersStone/thePhilosophersStone/Source/thePhilosophersStone/MovingPlatform.h"
+ )
+
